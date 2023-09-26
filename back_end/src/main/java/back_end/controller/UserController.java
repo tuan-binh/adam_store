77c1637fb9cdc4f8c8c6d932d243b86a709d67c1
tuan-binh <binh1892001@gmail.com>
2023-09-26 17:02:32 +0700

    up

--- back_end/src/main/java/back_end/controller/UserController.java
+++ back_end/src/main/java/back_end/controller/UserController.java
@@ -2,6 +2,7 @@ package back_end.controller;
 
 import back_end.exception.CustomException;
 import back_end.model.dto.request.UserUpdate;
+import back_end.model.dto.response.ProductResponse;
 import back_end.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,16 +21,31 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 	
+	@GetMapping("/favourite")
+	public ResponseEntity<List<ProductResponse>> getFavourite(Authentication authentication) {
+		return null;
+	}
+	
+	@PostMapping("/{productId}/favourite")
+	public ResponseEntity<List<ProductResponse>> addProductToFavourite(@PathVariable Long productId, Authentication authentication) {
+		return null;
+	}
+	
+	@DeleteMapping("/{productId}/favourite")
+	public ResponseEntity<List<ProductResponse>> removeProductInFavourite(@PathVariable Long productId, Authentication authentication) {
+		return null;
+	}
+	
 	@PutMapping("/update")
 	public ResponseEntity<String> updateInformation(@RequestBody UserUpdate userUpdate, Authentication authentication) throws CustomException {
-		userService.updateInformation(userUpdate,authentication);
+		userService.updateInformation(userUpdate, authentication);
 		return new ResponseEntity<>("update your information successfully", HttpStatus.OK);
 	}
 	
 	@PutMapping("/password")
-	public ResponseEntity<String> changePassword(@RequestParam Optional<String> password,Authentication authentication) throws CustomException {
-		userService.changePassword(password,authentication);
-		return new ResponseEntity<>("update password successfully",HttpStatus.OK);
+	public ResponseEntity<String> changePassword(@RequestParam Optional<String> password, Authentication authentication) throws CustomException {
+		userService.changePassword(password, authentication);
+		return new ResponseEntity<>("update password successfully", HttpStatus.OK);
 	}
 	
 }
